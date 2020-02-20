c290f3d7757c5793621d8ad5408ff23cf56f9e21
Alex Olivas <alex.r.olivas@gmail.com>
2020-02-20 15:57:40 -0700

    added test for voight_kampff

---
+++ tests/test_voight_kampff.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+import random
+from math import sqrt
+from math import isnan
+
+import numpy
+
+import voka.voight_kampff
+
+class TestVoightKampff(unittest.TestCase):
+
+    def setUp(self):
+        mu = 0.
+        sigma = 1.
+
+        histogram_names = ["Histogram%d" % i for i in range(100)]
+        self.test_hist = dict()
+        for name in histogram_names:
+            dist = [random.gauss(mu, sigma) for _ in range(1000)]
+            self.test_hist[name] = numpy.histogram(dist)[0]
+
+            
+        reference_names = ["ReferenceRun%d" % i for i in range(5)]
+        self.reference_collection = {name:dict() for name in reference_names}
+        for reference_name in reference_names:
+            # For each run generate a set of histograms
+            # with the same structure and names as the test histograms
+            
+            for name in histogram_names:
+                dist = [random.gauss(mu, sigma) for _ in range(1000)]
+                self.reference_collection[reference_name][name] = numpy.histogram(dist)[0]
+                           
+    def test_voight_kampff(self):
+        vk = voka.voight_kampff.VoightKampff(self.reference_collection)
+        result = vk.go(self.test_hist)
+        #self.assertFalse(isnan(result))
+
+unittest.main()
+
--- voka/voight_kampff.py
+++ voka/voight_kampff.py
@@ -1,16 +1,18 @@
+import voka.lof
 
 class VoightKampff(object):
 
-    def __init__(self, reference):
-        self.__reference = reference
+    def __init__(self, reference_collection):
         # the reference should be a collection (iterable)
         # of reference dictionaries.
-        # Reference {'', [[], []]}
+        # Reference {'', {'', []}}
         # Test {'', []}
-
+        self.__reference_collection = reference_collection
+        self.__k = 3
+        
     def determine_parameters(self):
         # calculate the thresholds from
-        # the benchmark set
+        # the reference set
         # we should also be able to determine
         # a reasonable k-distance
         pass
@@ -20,5 +22,17 @@ class VoightKampff(object):
         # the benchmark set
         # we should also be able to determine
         # a reasonable k-distance
-        pass
+        result = dict()
+        for test_key, test_sequence in test.items():
+
+            # pull the reference sequences out of the collection
+            reference_sequences = list()
+            for reference_name, reference_set in self.__reference_collection.items():
+                if test_key in reference_set:
+                    reference_sequences.append(reference_set[test_key])
+                    
+            result[test_key] = voka.lof.LOF(test_sequence,
+                                            self.__k,
+                                            reference_sequences)
+        return result
     
